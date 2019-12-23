e16d783be8930dcf449de9b709bee7be2aeea432
Monty <tuckerjsarah@gmail.com>
2019-12-23 16:55:05 -0500

    outlining

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Nav from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Nav/>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Goal Setting 101: Be Specific Measurable Attainable Relevant Time-based
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
